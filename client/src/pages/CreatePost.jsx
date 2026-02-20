df375efc8bafe9048ff2861047e767d2301b1e66
ThomasDale1 <dalethomas1212@gmail.com>
2026-02-19 21:55:29 -0600

    side menu features done from client side with dummy data except for Creat Post

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { dummyUserData } from '../assets/assets'
 
 const CreatePost = () => {
+  const [content, setContent] = useState('')
+  const [images, setImages] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const user = dummyUserData
+
   return (
-    <div>CreatePost</div>
+    <div className='min-h-screen bg-linear-to-b from-slate-50 to-white'>
+      <div className='max-w-6xl mx-auto p-6'>
+        <div className='mb-8'>
+          <h1 className="text-3xl font-bold text-slate-900 mb-2">Create Post</h1>
+          <p className="text-slate-600">Share your thoughts with the world</p>
+        </div>
+        <div className="max-w-xl bg-white p-4 sm:p-8 sm:pb-3 rounded-xl shadow-md space-y-4">
+          <div className='flex flex-items gap-3'>
+            <img src={user.profile_picture} className="w-12 h-12 rounded-full shadow" />
+            <div>
+              <h2 className='font-semibold'>{user.full_name}</h2>
+              <p className='text-sm text-gray-500'>@{user.username}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/src/pages/Messages.jsx
+++ client/src/pages/Messages.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { dummyConnectionsData } from '../assets/assets'
 import { Eye, MessageSquare } from 'lucide-react'
+import {useNavigate} from 'react-router-dom'
 
 const Messages = () => {
+  const navigate = useNavigate()
   return (
-    <div className='min-h-screen relative bg-slate-50 flex justify-center'>
-      <div className='max-w-6xl p-6'>
+    <div className='min-h-screen relative bg-slate-50'>
+      <div className='max-w-6xl p-6 mx-auto'>
         <div className='mb-8'>
           <h1 className='text-3xl font-bold text-slate-900 mb-2'>Messages</h1>
           <p className='text-slate-600'>Talk to your friends and family</p>
         </div>
-        <div className="flex flex-col gap-3 items-center">
+        <div className="flex flex-col gap-3">
           {dummyConnectionsData.map((user)=>(
             <div key={user._id} className='max-w-xl flex gap-5 p-6 bg-white shadow rounded-md'>
               <img src={user.profile_picture} className="rounded-full size-12 mx-auto"/>
@@ -20,10 +22,10 @@ const Messages = () => {
                 <p className='text-sm text-gray-600'>{user.bio}</p>
               </div>
               <div className='flex flex-col gap-2 mt-4'>
-                <button className='size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer gap-1'>
+                <button onClick={()=>navigate(`/messages/${user._id}`)} className='size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer gap-1'>
                   <MessageSquare className='w-4 h-4'/>
                 </button>
-                <button className='size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer'>
+                <button onClick={()=>navigate(`/profile/${user._id}`)} className='size-10 flex items-center justify-center text-sm rounded bg-slate-100 hover:bg-slate-200 text-slate-800 active:scale-95 transition cursor-pointer'>
                   <Eye className='w-4 h-4'/>
                 </button>
               </div>
