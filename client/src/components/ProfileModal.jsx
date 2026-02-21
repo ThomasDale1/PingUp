77268af1c61406fed741ee211a28175098922547
ThomasDale1 <dalethomas1212@gmail.com>
2026-02-21 13:29:39 -0600

    client side completed

--- client/src/components/ProfileModal.jsx
+++ client/src/components/ProfileModal.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { dummyUserData } from '../assets/assets'
 import { Pencil } from 'lucide-react'
 
-const ProfileModal = () => {
+const ProfileModal = ({setShowEdit}) => {
     const user = dummyUserData
     const [editForm, setEditForm] = useState({
         username: user.username,
@@ -71,8 +71,8 @@ const ProfileModal = () => {
                         </label>
                     </div>
                     <div className="flex justify-end space-x-3 pt-6">
-                        <button className='px-4 py-2 border border-gray-300 rounded-lg text-gray-700 hover:bg-gray-50 transition-colors'>Cancel</button>
-                        <button className='px-4 py-2 bg-linear-to-r from-indigo-500 to-purple-600 text-white rounded-lg hover:from-indigo-600 hover:to-purple-700 transition cursor-pointer'>Save Changes</button>
+                        <button type='button' onClick={()=>setShowEdit(false)} className='px-4 py-2 border border-gray-300 rounded-lg text-gray-700 hover:bg-gray-50 transition-colors cursor-pointer'>Cancel</button>
+                        <button type='submit' className='px-4 py-2 bg-linear-to-r from-indigo-500 to-purple-600 text-white rounded-lg hover:from-indigo-600 hover:to-purple-700 transition cursor-pointer'>Save Changes</button>
                     </div>
                 </form>
             </div>
