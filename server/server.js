0b7a61f1a00c08fa2b65129bf79f387cfbd67c62
ThomasDale1 <dalethomas1212@gmail.com>
2026-02-24 19:28:34 -0600

    backend completed

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ import {inngest, functions} from './inngest/index.js'
 import { serve } from "inngest/express";
 import { clerkMiddleware } from '@clerk/express'
 import userRouter from './routes/userRoutes.js'
+import postRouter from './routes/postRoutes.js'
+import storyRouter from './routes/storyRoutes.js'
+import messageRouter from './routes/messageRoutes.js'
 
 const app = express()
 
@@ -16,6 +19,9 @@ app.use(cors())
 app.use(clerkMiddleware())
 app.use('/api/inngest', serve({client: inngest, functions}))
 app.use('/api/user', userRouter)
+app.use('/api/post', postRouter)
+app.use('/api/story', storyRouter)
+app.use('/api/message', messageRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello Express')
